53123a3839378eeadb62568346d2b6da4ef1449c
adamhinton <adam.d.hinton@gmail.com>
2022-05-06 22:57:59 -0400

    declare h1 variable

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,6 +1,9 @@
 const Todo = (props) => {
   const { id, title, completed } = props.todo;
 
+  const h1 = <h1>{title}</h1>;
+  const text = completed ? <strike>{h1}</strike> : h1;
+
   return <div data-testid="todo-1">{title}</div>;
 };
 
