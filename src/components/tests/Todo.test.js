5ecc003bc962b0e779225c9f801ed58bd25d82ff
adamhinton <adam.d.hinton@gmail.com>
2022-05-06 22:38:02 -0400

    expect(todoElement).toHaveTextContent("Hello World!");

--- src/components/tests/Todo.test.js
+++ src/components/tests/Todo.test.js
@@ -6,4 +6,5 @@ test("should render todo component", () => {
 
   const todoElement = screen.getByTestId("todo-1");
   expect(todoElement).toBeInTheDocument();
+  expect(todoElement).toHaveTextContent("Hello World!");
 });
