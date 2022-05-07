a2bfab9cd9aa53b59b371ad8473e6173c071e515
adamhinton <adam.d.hinton@gmail.com>
2022-05-06 22:57:01 -0400

    pass todo object in to Todo as props

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
   return (
     <div className="App">
       {todos.map((todo) => {
-        return <Todo todo={todo} />;
+        return <Todo todo={todo} key={todo.id} />;
       })}
     </div>
   );
--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,7 +1,7 @@
 const Todo = (props) => {
-  const { id, title, completed } = props;
+  const { id, title, completed } = props.todo;
 
-  return <div data-testid="todo-1">Hello World!</div>;
+  return <div data-testid="todo-1">{title}</div>;
 };
 
 export default Todo;
