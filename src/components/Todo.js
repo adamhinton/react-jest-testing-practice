23d7c6dd3e7f7e73ee4021b15ba391633ccba792
adamhinton <adam.d.hinton@gmail.com>
2022-05-06 22:58:55 -0400

    make dynamic testids in Todo

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -4,7 +4,7 @@ const Todo = (props) => {
   const h1 = <h1>{title}</h1>;
   const text = completed ? <strike>{h1}</strike> : h1;
 
-  return <div data-testid="todo-1">{title}</div>;
+  return <div data-testid={`todo-${id}`}>{text}</div>;
 };
 
 export default Todo;
