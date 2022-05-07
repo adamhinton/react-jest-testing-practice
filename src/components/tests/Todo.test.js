167b91adbaae939f13fab66a1c5b4408e5f6b649
adamhinton <adam.d.hinton@gmail.com>
2022-05-06 23:06:05 -0400

    update test description

--- src/components/tests/Todo.test.js
+++ src/components/tests/Todo.test.js
@@ -1,7 +1,7 @@
 import { render, screen, cleanup } from "@testing-library/react";
 import Todo from "../Todo";
 
-test("should render todo component", () => {
+test("should render non-completed Todo component", () => {
   const todo = { id: 1, title: "wash dishes", completed: false };
 
   render(<Todo todo={todo} />);
