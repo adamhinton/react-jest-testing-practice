120a72a8bade9d29fca34571ee812cfc1bb8c211
adamhinton <adam.d.hinton@gmail.com>
2022-05-06 23:07:13 -0400

    write second test for completed todo

--- src/components/tests/Todo.test.js
+++ src/components/tests/Todo.test.js
@@ -1,7 +1,7 @@
 import { render, screen, cleanup } from "@testing-library/react";
 import Todo from "../Todo";
 
-test("should render non-completed Todo component", () => {
+test("[1] should render non-completed Todo component", () => {
   const todo = { id: 1, title: "wash dishes", completed: false };
 
   render(<Todo todo={todo} />);
@@ -9,4 +9,14 @@ test("should render non-completed Todo component", () => {
   const todoElement = screen.getByTestId("todo-1");
   expect(todoElement).toBeInTheDocument();
   expect(todoElement).toHaveTextContent("wash dishes");
+});
+
+test("[2] should render non-completed Todo component", () => {
+  const todo = { id: 2, title: "wash car", completed: true };
+
+  render(<Todo todo={todo} />);
+
+  const todoElement = screen.getByTestId("todo-2");
+  expect(todoElement).toBeInTheDocument();
+  expect(todoElement).toHaveTextContent("wash car");
 });
