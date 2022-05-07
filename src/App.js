9ee4ea02626a74d18724d76be7ae3b4a5b993e2d
adamhinton <adam.d.hinton@gmail.com>
2022-05-06 22:53:00 -0400

    make todo list in App.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,11 @@ import "./App.css";
 import Todo from "./components/Todo";
 
 function App() {
+  const todos = [
+    { id: 1, title: "wash dishes", completed: false },
+    { id: 2, title: "make dinner", completed: true },
+  ];
+
   return (
     <div className="App">
       <Todo />
